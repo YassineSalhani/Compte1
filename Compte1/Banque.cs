9db59fc8ed6fc105f19238a926610e62759a2525
salhani_y <salhani_y@D29-022024-05>
2022-09-09 15:07:41 +0200

    Ajoutez des fichiers projet.

---
+++ Compte1/Banque.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte1
+{
+    class Banque
+    {
+        public Banque()
+        {
+            mesComptes = new List<Compte>();
+        }
+        private List<Compte> mesComptes;
+
+        public List<Compte> MesComptes
+        {
+            get { return mesComptes; }
+            set { mesComptes = value; }
+        }
+
+
+        public void ajouter(Compte c)
+        {
+            mesComptes.Add(c);
+        }
+        public void ajouter(int leNumero, string leNom, double leSolde, double leDecouvertAutorise)
+        {
+            Compte monCompte = new Compte(leNumero, leNom, leSolde, leDecouvertAutorise);
+            this.ajouter(monCompte);
+        }
+        public Compte rendCompte(int numCompte)
+        {
+            foreach (Compte c in mesComptes)
+            {
+                if (c.Contains(numCompte))
+                {
+                    return c;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+        }
+        
+
+
+
+    }
+
+
+}
---
+++ Compte1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banque b = new Banque();
+            Compte c1;
+            c1 = new Compte(12345, "toto", 1000, -500);
+            b.ajouter(c1);
+            b.ajouter(12345, "toto", 1000, -500);
+            b.ajouter(45657, "titi", 2000, -1000);
+            b.ajouter(12654, "tintin", 5000, -500);
+            Console.WriteLine(b.rendCompte(45657).ToString());
+
+            foreach (Compte c in b.MesComptes)
+            {
+                Console.WriteLine(c.ToString());
+            }
+
+        }
+    }
+}
